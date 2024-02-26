e3ffaf13d67570fd5f4639fef721d533791eeb21
greisyvirgen <virgenlg@oregonstate.edu>
2024-02-25 18:59:00 -0800

    Changed back to original port

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = create_app()
 
 # Listener
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 12119))
+    port = int(os.environ.get('PORT', 12118))
     app.run(port=port, debug=True)
